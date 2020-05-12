cc436165ad21646e74f3410d79e096cc3635f927
Thomas <t.chezieres@gmail.com>
2020-05-12 14:20:26 +0100

    Including Camera Smooth Follow script
    The camera will smoothly follow the target attached to it.
    You can impose an offset for the camera position (Vector3)

---
+++ Camera Scripts/SmoothFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+	void Start()
+	{
+	}
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+		transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+    }
+}
